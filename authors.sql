a5087eaf7804d2339254fd8697872e4b6941133e
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-23 17:17:07 +0200

    authors

--- authors.sql
+++ authors.sql
@@ -13,5 +13,11 @@ CREATE TABLE
             updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW() 
 );
 
-INSERT INTO authors(name, last_name, birth_year, country) VALUES('Dan', 'Brown', '22-06-1964', 'American');
+INSERT INTO authors(name, last_name, birth_year, country) VALUES('Dan', 'Brown', '22-06-1964', 'America');
+INSERT INTO authors(name, last_name, birth_year, country) VALUES('Agatha', 'Christie', '25-05-1963', 'Spain');
+INSERT INTO authors(name, last_name, birth_year, country) VALUES('Stephen', 'Hauking', '23-03-1977', 'Italy');
+INSERT INTO authors(name, last_name, birth_year, country) VALUES('John', 'Grisham', '01-05-1954', 'America');
+INSERT INTO authors(name, last_name, birth_year, country) VALUES('James', 'Patterson', '12-10-1940', 'Germany');
+INSERT INTO authors(name, last_name, birth_year, country) VALUES('Haruki', 'Murakami', '11-09-1988', 'Sweden');
+INSERT INTO authors(name, last_name, birth_year, country) VALUES('Ken', 'Follett', '02-09-1954', 'Ethiopia');
 
