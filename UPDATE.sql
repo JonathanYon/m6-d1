0d2b9aba7cb8cd13bfd79b730c138052bb00470d
Yonanni <84571687+JonathanYon@users.noreply.github.com>
2021-08-23 16:26:12 +0200

    UPDATE

---
+++ UPDATE.sql
@@ -0,0 +1,2 @@
+UPDATE books SET cover='https://media.fuzia.com/assets/uploads/images/co_brand_1/article/2017/1500484464120_the_mysterious_affair_at_styles.jpg' WHERE bookS_id=4 RETURNING *
+                
